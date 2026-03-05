f39d84d9e26c2f1d936867d469b0d25d011bf06e
Sahidulislam05 <sahidulislamcst@gmail.com>
2026-03-05 16:06:34 +0600

    Added sidebar

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -5,7 +5,7 @@ const BannerCard = ({ count, resolvedCount }) => {
     <div className="container mx-auto">
       <div className="flex flex-col md:flex-row justify-center gap-5 mt-16">
         <div className="relative md:w-full max-w-2xl rounded-xl overflow-hidden">
-          <div className="absolute inset-0 bg-gradient-to-r from-[#632EE3] to-[#9F62F2]" />
+          <div className="absolute inset-0 bg-linear-to-r from-[#632EE3] to-[#9F62F2]" />
 
           <div className="absolute top-0 left-0 h-full flex items-center">
             <img src={navImg} alt="" className="h-full " />
@@ -23,7 +23,7 @@ const BannerCard = ({ count, resolvedCount }) => {
           </div>
         </div>
         <div className="relative  md:w-full max-w-2xl rounded-xl overflow-hidden">
-          <div className="absolute inset-0 bg-gradient-to-r from-[#54CF68] to-[#00827A]" />
+          <div className="absolute inset-0 bg-linear-to-r from-[#54CF68] to-[#00827A]" />
 
           <div className="absolute top-0 left-0 h-full flex items-center">
             <img src={navImg} alt="" className="h-full " />
